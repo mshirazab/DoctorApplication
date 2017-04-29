a1e128b987d751003e6179a8da3acded455057a7
shiraz <mshirazab@gmail.com>
2017-04-29 22:34:18 +0530

    Fourth Commit : Added funtionality of signing in and out

--- app/src/main/java/com/project/asd/doctorapplication/MainActivity.java
+++ app/src/main/java/com/project/asd/doctorapplication/MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,7 +18,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -35,49 +39,53 @@ public class MainActivity extends AppCompatActivity {
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
 
-    private ListView mMessageListView;
-    private MessageAdapter mMessageAdapter;
-    private ProgressBar mProgressBar;
-    private ImageButton mPhotoPickerButton;
-    private EditText mMessageEditText;
-    private Button mSendButton;
+    private ListView messageListView;
+    private MessageAdapter messageAdapter;
+    private ProgressBar progressBar;
+    private ImageButton photoPickerButton;
+    private EditText messageEditText;
+    private Button sendButton;
 
     FirebaseDatabase database;
     DatabaseReference databaseReference;
     ChildEventListener eventListener;
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener authStateListener;
 
 
-    private String mUsername;
+    private String username;
     private static final int IMAGE_SELECT_ID = 187;
+    private static final int LOGGING_IN_ID = 760;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mUsername = ANONYMOUS;
+        username = ANONYMOUS;
 
         // Initialize references to views
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mMessageListView = (ListView) findViewById(R.id.messageListView);
-        mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
-        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
-        mSendButton = (Button) findViewById(R.id.sendButton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        messageListView = (ListView) findViewById(R.id.messageListView);
+        photoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
+        messageEditText = (EditText) findViewById(R.id.messageEditText);
+        sendButton = (Button) findViewById(R.id.sendButton);
 
         //Initialize the firebase database
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("message");
+        firebaseAuth = FirebaseAuth.getInstance();
 
         // Initialize message ListView and its adapter
         List<FriendlyMessage> friendlyMessages = new ArrayList<>();
-        mMessageAdapter = new MessageAdapter(this, R.layout.item_message, friendlyMessages);
-        mMessageListView.setAdapter(mMessageAdapter);
+        messageAdapter = new MessageAdapter(this, R.layout.item_message, friendlyMessages);
+        messageListView.setAdapter(messageAdapter);
 
         // Initialize progress bar
-        mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
 
         // ImagePickerButton shows an image picker to upload a image for a message
-        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+        photoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startImageSelector();
@@ -85,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Enable Send button when there's text to send
-        mMessageEditText.addTextChangedListener(new TextWatcher() {
+        messageEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -93,9 +101,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.toString().trim().length() > 0) {
-                    mSendButton.setEnabled(true);
+                    sendButton.setEnabled(true);
                 } else {
-                    mSendButton.setEnabled(false);
+                    sendButton.setEnabled(false);
                 }
             }
 
@@ -103,46 +111,84 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
             }
         });
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+        messageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
 
-        mSendButton.setOnClickListener(new View.OnClickListener() {
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                databaseReference.push().setValue(new FriendlyMessage(mMessageEditText.getText().toString(), mUsername, null));
+                FriendlyMessage message = new FriendlyMessage(messageEditText.getText().toString(), username, null);
+                databaseReference.push().setValue(message);
             }
         });
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    onSignedIn(user.getDisplayName());
+                } else {
+                    onSignedOut();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setProviders(AuthUI.EMAIL_PROVIDER)
+                                    .build(), LOGGING_IN_ID);
+                }
+            }
+        };
+    }
+
+    //authorization funtions
+    private void onSignedIn(String name) {
+        username = name;
         displayMessages();
     }
 
+    private void onSignedOut() {
+        username = ANONYMOUS;
+        messageAdapter.clear();
+        removeMessages();
+    }
+
+    //database displaying and removing funtions
     void displayMessages() {
-        eventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                FriendlyMessage friendlymessage = dataSnapshot.getValue(FriendlyMessage.class);
-                mMessageAdapter.add(friendlymessage);
-            }
+        if (eventListener == null) {
+            eventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    FriendlyMessage friendlymessage = dataSnapshot.getValue(FriendlyMessage.class);
+                    messageAdapter.add(friendlymessage);
+                }
 
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-            }
+                }
 
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-            }
+                }
 
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
-            }
+                }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-            }
-        };
-        databaseReference.addChildEventListener(eventListener);
+                }
+            };
+            databaseReference.addChildEventListener(eventListener);
+        }
+    }
+
+    void removeMessages() {
+        if (eventListener != null) {
+            databaseReference.removeEventListener(eventListener);
+            eventListener = null;
+        }
     }
 
     void startImageSelector() {
@@ -157,6 +203,24 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == IMAGE_SELECT_ID && resultCode == RESULT_OK) {
             Uri uri = data.getData();
         }
+        if (requestCode == LOGGING_IN_ID && resultCode == RESULT_CANCELED) {
+            finish();
+        }
+        if (requestCode == LOGGING_IN_ID && resultCode == RESULT_OK) {
+            Toast.makeText(this, "Signed in successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(authStateListener);
     }
 
     @Override
@@ -168,6 +232,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu: {
+                AuthUI.getInstance().signOut(this);
+                break;
+            }
+        }
         return super.onOptionsItemSelected(item);
     }
 }
